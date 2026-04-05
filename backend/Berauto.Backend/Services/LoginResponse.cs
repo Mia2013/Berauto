560e077c546961b614cfc7127fd3f6098d1d1ad7
Ónodi Norbert <YQXJNV@student.uni-pannon.hu>
2026-04-05 20:36:07 +0200

    LoginResult és LoginResponse hozzáadása.

---
+++ backend/Berauto.Backend/Services/LoginResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; }
+        public DateTime Expires { get; set; }
+        public string Role { get; set; }
+        public String Name { get; set; }
+    }
+}
---
+++ backend/Berauto.Backend/Services/LoginResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public enum Status
+    {
+        AlreadyExists,
+        NotExists,
+        Success,
+        InvalidPassword
+    }
+   public  class LoginResult
+    {
+        public Status status { get;set; }
+        public LoginResponse? Response { get; set; }
+    }
+}
