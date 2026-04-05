d0cdb44533219e2de5e0f43f4d29682d9e8b4747
Ónodi Norbert <YQXJNV@student.uni-pannon.hu>
2026-04-05 20:24:07 +0200

    RegisterDto és LoginDto létrehozása

---
+++ backend/Berauto.Backend/Berauto.Backend/DTOs/RegisterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Berauto.Backend.DTOs
+{
+    public class RegisterDto
+    {
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
