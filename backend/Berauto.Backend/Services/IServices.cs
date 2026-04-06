50ca4cfdb3a7f85941212c7adba2da8498775702
Ónodi Norbert <YQXJNV@student.uni-pannon.hu>
2026-04-06 21:12:48 +0200

    IService javítva.

--- backend/Berauto.Backend/Services/IServices.cs
+++ backend/Berauto.Backend/Services/IServices.cs
@@ -8,10 +8,10 @@ namespace Services
 {
     public interface IServices<TEntity,TDto>
     {
-        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<IEnumerable<TEntity>> GetAllAsync(Func<IQueryable<TEntity>,IQueryable<TEntity>>incule=null);
         Task<TEntity> GetEntityById(int id);
-        Task AddEntity(Func<TDto, TEntity> createEntity);
-        Task UpdateEntity(int id, TDto dto, Action<TDto, TEntity> updateEntity);
-        Task DeleteEntity(int id);
+        Task<bool> AddEntity(TDto dto,Func<TDto, TEntity> createEntity);
+        Task<bool> UpdateEntity(int id, TDto dto, Action<TDto, TEntity> updateEntity);
+        Task<bool> DeleteEntity(int id);
     }
 }
