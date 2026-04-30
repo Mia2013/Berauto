797daaba229f18f5838f88167184c95457919870
Ónodi Norbert <YQXJNV@student.uni-pannon.hu>
2026-04-30 11:22:34 +0200

    User tábla kiegészítése username és passwordhash mezőkkel.

--- backend/Berauto.Backend/Berauto.Backend/Models/User.cs
+++ backend/Berauto.Backend/Berauto.Backend/Models/User.cs
@@ -10,8 +10,10 @@ public partial class User
     public int RoleId { get; set; }
 
     public string Name { get; set; } = null!;
+    public string? Username { get; set; }
 
     public string Email { get; set; } = null!;
+    public string? PasswordHsh { get; set; }
 
     public string? Phone { get; set; }
 
