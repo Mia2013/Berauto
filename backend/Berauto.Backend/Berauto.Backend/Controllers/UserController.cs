821a35d94698feb6df44c4f9bb61710577522c72
Ónodi Norbert <YQXJNV@student.uni-pannon.hu>
2026-05-06 19:58:00 +0200

    Usercontroller hozzáadása, személyes adatok lekérése,módosítása.

---
+++ backend/Berauto.Backend/Berauto.Backend/Controllers/UserController.cs
@@ -0,0 +1,71 @@
+﻿using Berauto.Backend.Data;
+using Berauto.Backend.DTO;
+using Berauto.Backend.Models;
+using Berauto.Backend.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace Berauto.Backend.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+        public UserController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<User>> GetMyData()
+        {
+            var userid = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (userid == null)
+            {
+                return Unauthorized();
+            }
+            var user = await _unitOfWork.Users.FindByIdAsync(Int32.Parse(userid));
+          
+            return Ok(user);
+        }
+        [HttpPut]
+        public async Task<ActionResult>UpdateUserData(UpdateUserDto dto)
+        {
+
+            var userid = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (userid == null)
+            {
+                return Unauthorized();
+            }
+            var user = await _unitOfWork.Users.FindByIdAsync(Int32.Parse(userid));
+            if(dto.Password!=null)
+            {
+                user.PasswordHash = PassHashing(dto, user);
+            }
+            if(dto.Phone!=null)
+            {
+                user.Phone = dto.Phone;
+            }
+            if(dto.Address!=null)
+            {
+                user.Address = dto.Address;
+            }
+            _unitOfWork.Users.Update(user);
+            return Ok();
+        }
+        public String PassHashing(UpdateUserDto dto, User User)
+        {
+
+            var passwordhash = new PasswordHasher<User>();
+            return passwordhash.HashPassword(User, dto.Password);
+
+        }
+    }
+}
---
+++ backend/Berauto.Backend/Berauto.Backend/DTO/UpdateUserDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Berauto.Backend.DTO
+{
+    public class UpdateUserDto
+    {
+        public string? Password { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? DrivingLicence { get; set; }
+
+    }
+}
